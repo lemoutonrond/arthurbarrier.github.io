25209e3d0c259c0d8d6ac1541d81eade35b15d2d
lemoutonrond <arthurbarrier01@gmail.com>
2024-06-12 13:42:49 +0200

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,38 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'bouche 1.png') {
+    myImage.setAttribute ('src','bouche 2.png');
+  } else {
+    myImage.setAttribute ('src','bouche 1.png');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'T qui soldat ? Je suis ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'T qui soldat ? Je suis ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
